0344e42ca2ec9bdda6a4f8d323d08802cbb54049
Bishoy <bishoy.samwuel@gmail.com>
2022-02-16 16:21:40 +0200

    Add Hero Section elements Title, Text, Buton

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,20 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center >
+        Welcome To <br />
+        My Personal Portfolio
+      </SectionTitle>
+      <SectionText>
+        My goal is to add more value to the team I belong to using clean code.
+      </SectionText>
+      <Button onClick={() => window.location = '//'}>
+        My Resume
+      </Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
