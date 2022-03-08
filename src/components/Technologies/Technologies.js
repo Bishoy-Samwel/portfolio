fa8b7ff45384168595f715f037ffb10b44e3d940
Bishoy <bishoy.samwuel@gmail.com>
2022-03-08 12:21:45 +0200

    Add Technologies structure and styles

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -3,10 +3,27 @@ import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
 import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
-const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+const Technologies = () => (
+  <Section id="tech">
+    <SectionDivider />
+    <SectionTitle> Technologies </SectionTitle>
+    <SectionText>
+        I've worked with a range of technologies between Front end and Back end
+    </SectionText>
+  <List>
+    <ListItem>
+    <DiFirebase size="3rem" />
+      <ListContainer>
+        <ListTitle>Front-end</ListTitle>
+        <ListParagraph>
+          Experience with <br />
+          React.js
+        </ListParagraph>
+      </ListContainer>
+    </ListItem>
+  </List>
+  </Section>
+
 );
 
 export default Technologies;
