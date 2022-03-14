481322bb541f29f86f8ff6ab80685054d860320a
Bishoy <bishoy.samwuel@gmail.com>
2022-03-14 16:40:53 +0200

    Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,9 +6,32 @@ import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTi
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <FooterWrapper>
+      <LinkList>
+        <LinkColumn>
+          <LinkTitle>Call</LinkTitle>
+          <LinkItem href="tel:+201097316708">+201097316708</LinkItem>
+        </LinkColumn>
+        <LinkColumn>
+          <LinkTitle>Email</LinkTitle>
+          <LinkItem href="bishoy.samwuel@gmail.com">Contact me on: bishoy.samwuel@gmail.com </LinkItem>
+        </LinkColumn>
+      </LinkList>
+      <SocialIconsContainer>
+        <CompanyContainer>
+          <Slogan>Innovating one project at a time</Slogan>
+        </CompanyContainer>
+        <SocialIcons href='/'>
+          <AiFillGithub size="3rem" />
+        </SocialIcons>
+        <SocialIcons href='/'>
+          <AiFillLinkedin size="3rem" />
+        </SocialIcons>
+        <SocialIcons href='/'>
+          <AiFillInstagram size="3rem" />
+        </SocialIcons>
+      </SocialIconsContainer>
+    </FooterWrapper>
   );
 };
 
