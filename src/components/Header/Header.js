2f12fac82a7c909baacca253da50fe0093f1ec58
Bishoy <bishoy.samwuel@gmail.com>
2022-03-15 16:47:22 +0200

    Header links

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,10 @@
 import Link from 'next/link';
 import React from 'react';
 import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+import { FaAngellist } from 'react-icons/fa';
+import { GiMuscleUp } from 'react-icons/gi';
+
+
 import { DiCssdeck } from 'react-icons/di';
 
 import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
@@ -10,8 +14,7 @@ const Header = () => (
     <Div1>
       <Link href="">
         <a style={{ display: "flex", alignItems: "center", color: "white" }}>
-          <DiCssdeck size="3rem" />
-          <span>Portofolio</span>
+          <GiMuscleUp size="6rem" />
         </a>
       </Link>
     </Div1>
@@ -33,14 +36,14 @@ const Header = () => (
       </li>
     </Div2>
     <Div3>
-      <SocialIcons href='/'>
+      <SocialIcons href='https://github.com/Bishoy-Samwel'>
         <AiFillGithub size="3rem" />
       </SocialIcons>
-      <SocialIcons href='/'>
+      <SocialIcons href='https://www.linkedin.com/in/bishoy-samwuel-ss'>
         <AiFillLinkedin size="3rem" />
       </SocialIcons>
-      <SocialIcons href='/'>
-        <AiFillInstagram size="3rem"/>
+      <SocialIcons href='https://angel.co/u/bishoy-samwel'>
+        <FaAngellist size="3rem"/>
       </SocialIcons>
     </Div3>
   </Container>
