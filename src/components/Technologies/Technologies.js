3437289da8e8d814bab5413b234084304efb4d1d
Bishoy <bishoy.samwuel@gmail.com>
2022-03-29 15:08:16 +0200

    Show the logos in technologies section

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,27 +1,77 @@
 import React from 'react';
-import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
+import { DiFirebase, DiReact, DiRubyRough, DiVisualstudio  } from 'react-icons/di';
+import { SiRubyonrails } from "react-icons/si";
+
 import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
+import { BackendLogos, FrontendLogos, ToolsLogos } from '../../constants/constants';
 
 const Technologies = () => (
   <Section id="tech">
     <SectionDivider />
     <SectionTitle main> Technologies </SectionTitle>
     <SectionText>
-        I've worked with a range of technologies between Front end and Back end
+
     </SectionText>
-  <List>
-    <ListItem>
-    <DiFirebase size="3rem" />
-      <ListContainer>
-        <ListTitle>Front-end</ListTitle>
-        <ListParagraph>
-          Experience with <br />
-          React.js
-        </ListParagraph>
-      </ListContainer>
-    </ListItem>
-  </List>
+    <List>
+      <ListItem>
+        <DiReact size="3rem" />
+        <ListContainer>
+          <ListTitle>Front-end</ListTitle>
+          <ListParagraph>
+          <>
+              {FrontendLogos.map((logo) => (
+                <a target={logo.a.target} rel={logo.a.rel} href={logo.href}>
+                  <img
+                    alt={logo.img.a}
+                    src={logo.img.src}
+                  />
+                </a>
+              )
+              )}
+            </>
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiRubyRough size="3rem" />
+        <ListContainer>
+          <ListTitle>Back-end</ListTitle>
+          <ListParagraph>
+            <>
+              {BackendLogos.map((logo) => (
+                <a target={logo.a.target} rel={logo.a.rel} href={logo.href}>
+                  <img
+                    alt={logo.img.a}
+                    src={logo.img.src}
+                  />
+                </a>
+              )
+              )}
+            </>
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiVisualstudio size="3rem" />
+        <ListContainer>
+          <ListTitle>Tools & Software</ListTitle>
+          <ListParagraph>
+          <>
+              {ToolsLogos.map((logo) => (
+                <a target={logo.a.target} rel={logo.a.rel} href={logo.href}>
+                  <img
+                    alt={logo.img.a}
+                    src={logo.img.src}
+                  />
+                </a>
+              )
+              )}
+            </>
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+    </List>
   </Section>
 
 );
