da777c11c6a2fac12aacb97e17ad99fde30324ba
Bishoy <bishoy.samwuel@gmail.com>
2022-03-29 15:07:38 +0200

    Add logos constants details

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -44,3 +44,363 @@ export const TimeLineData = [
   { year: 2020, text: 'Shared my projects with the world', },
   { year: 2021, text: 'Started my own platform', },
 ];
+
+export const BackendLogos = [
+  // {
+  //   a: {
+  //     target: '_blank', rel: 'noopener noreferrer',
+  //     href: 'https://www.vercel.com/'
+  //   }, img: {
+  //     alt: 'Vercel',
+  //     src: 'https://img.shields.io/badge/Vercel-00C7B7?style=for-the-badge&logo=vercel&logoColor=white'
+  //   }
+  // }
+  // , {
+  //   a: { target: '_blank', rel: 'noopener noreferrer', href: 'https://graphql.org/' }
+  //   , img: {
+  //     alt: 'Graphql',
+  //     src: 'https://img.shields.io/badge/GraphQL-e535ab?style=for-the-badge&logo=graphql&logoColor=white'
+  //   }
+  // }
+  // , {
+  //   a: {
+  //     target: '_blank', rel: 'noopener noreferrer',
+  //     href: 'https://firebase.google.com/'
+  //   }
+  //   , img: {
+  //     alt: 'Firebase',
+  //     src: 'https://img.shields.io/badge/Firebase-039BE5?style=for-the-badge&logo=firebase&logoColor=white,'
+  //   }
+  // }.
+  {
+    a: {
+      target: '_blank', rel: 'noopener noreferrer',
+      href: 'https://rubyonrails.org/'
+    }
+    , img: {
+      alt: 'Rails',
+      src: 'https://img.shields.io/badge/Ruby_on_Rails-CC0000?style=for-the-badge&logo=ruby-on-rails&logoColor=white'
+    }
+  }
+  , {
+    a: {
+      target: '_blank', rel: 'noopener noreferrer',
+      href: 'https://www.sqlite.org/'
+    }
+    , img: {
+      alt: 'Sqlite',
+      src: 'https://img.shields.io/badge/SQLite-07405E?style=for-the-badge&logo=sqlite&logoColor=white'
+    }
+  }
+  , {
+    a: {
+      target: '_blank', rel: 'noopener noreferrer',
+      href: 'https://www.netlify.com/'
+    }
+    , img: {
+      alt: 'Netlify',
+      src: 'https://img.shields.io/badge/Netlify-00C7B7?style=for-the-badge&logo=netlify&logoColor=white',
+    }
+  }
+  , {
+    a: { target: '_blank', rel: 'noopener noreferrer', href: 'https://nodejs.org/' }
+    , img: {
+      alt: 'Nodejs',
+      src: 'https://img.shields.io/badge/Node.js-43853D?style=for-the-badge&logo=node.js&logoColor=white'
+    }
+  }
+
+  , {
+    a: {
+      target: '_blank', rel: 'noopener noreferrer',
+      href: 'https://www.mysql.com/',
+    }
+    , img: {
+      alt: 'Mysql',
+      src: 'https://img.shields.io/badge/MySQL-00000F?style=for-the-badge&logo=mysql&logoColor=white,'
+    }
+  }
+  , {
+    a: {
+      target: '_blank', rel: 'noopener noreferrer',
+      href: 'https://www.postgresql.org/'
+    }
+    , img: {
+      alt: 'Postgresql',
+      src: 'https://img.shields.io/badge/PostgreSQL-316192?style=for-the-badge&logo=postgresql&logoColor=white'
+    }
+  }
+  , {
+    a: {
+      target: '_blank', rel: 'noopener noreferrer',
+      href: 'https://www.ruby-lang.org/en/'
+    }
+    , img: {
+      alt: 'Ruby',
+      src: 'https://img.shields.io/badge/Ruby-A91401?style=for-the-badge&logo=ruby&logoColor=white'
+    }
+  }
+  , {
+    a: {
+      target: '_blank', rel: 'noopener noreferrer',
+      href: 'https://www.heroku.com/'
+    }
+    , img: {
+      alt: 'Heroku',
+      src: 'https://img.shields.io/badge/Heroku-430098?style=for-the-badge&logo=heroku&logoColor=white'
+    }
+  }
+]
+
+export const FrontendLogos = [
+  {
+    a: { target: '_blank', rel: 'noopener noreferrer', href: 'https://reactjs.org/' }
+    , img: {
+      alt: 'React',
+      src: 'https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB'
+    }
+  },
+  // {
+  //   a: {
+  //     target: '_blank',
+  //     rel: 'noopener noreferrer'
+  //     , href: 'https://tailwindcss.com/'
+  //   }
+  //   , img: {
+  //     alt: 'Tailwind',
+  //     src: 'https://img.shields.io/badge/Tailwind_CSS-38B2AC?style=for-the-badge&logo=tailwind-css&logoColor=white'
+  //   }
+  // },
+  {
+    a: {
+      target: '_blank',
+      rel: 'noopener noreferrer'
+      , href: 'https://www.javascript.com/'
+    }
+    , img: {
+      alt: 'JavaScript',
+      src: 'https://img.shields.io/badge/JavaScript-323330?style=for-the-badge&logo=javascript&logoColor=F7DF1E'
+    }
+  },
+  {
+    a: {
+      target: '_blank',
+      rel: 'noopener noreferrer'
+      , href: 'https://webpack.js.org/'
+    }
+    , img: {
+      alt: 'Webpack',
+      src: 'https://img.shields.io/badge/Webpack-8ccbf3?style=for-the-badge&logo=webpack&logoColor=white'
+    }
+  },
+  {
+    a: {
+      target: '_blank',
+      rel: 'noopener noreferrer'
+      , href: 'https://getbootstrap.com/'
+    }
+    , img: {
+      alt: 'Bootstrap',
+      src: 'https://img.shields.io/badge/Bootstrap-563D7C?style=for-the-badge&logo=bootstrap&logoColor=white'
+    }
+  },
+  {
+    a: {
+      target: '_blank',
+      rel: 'noopener noreferrer'
+      , href: 'https://material.io/design'
+    }
+    , img: {
+      alt: 'Material Design',
+      src: 'https://img.shields.io/badge/Material--UI-0081CB?style=for-the-badge&logo=material-ui&logoColor=white'
+    }
+  },
+  {
+    a: {
+      target: '_blank',
+      rel: 'noopener noreferrer'
+      , href: 'https://html.com/html5/'
+    }
+    , img: {
+      alt: 'HTML',
+      src: 'https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white'
+    }
+  },
+  {
+    a: {
+      target: '_blank',
+      rel: 'noopener noreferrer'
+      , href: 'https://www.w3.org/Style/CSS/Overview.en.html'
+    }
+    , img: {
+      alt: 'CSS',
+      src: 'https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white'
+    }
+  },
+  // {
+  //   a: {
+  //     target: '_blank',
+  //     rel: 'noopener noreferrer'
+  //     , href: 'https://sass-lang.com/'
+  //   }
+  //   , img: {
+  //     alt: 'Sass',
+  //     src: 'https://img.shields.io/badge/Sass-CC6699?style=for-the-badge&logo=sass&logoColor=white'
+  //   }
+  // },
+  {
+    a: { target: '_blank', rel: 'noopener noreferrer', href: 'https://nextjs.org/' }
+    , img: {
+      alt: 'Next',
+      src: 'https://img.shields.io/badge/Next-black?style=for-the-badge&logo=next.js&logoColor=white'
+    }
+  },
+  {
+    a: { target: '_blank', rel: 'noopener noreferrer', href: 'https://redux.js.org/' }
+    , img: {
+      alt: 'Redux',
+      src: 'https://img.shields.io/badge/Redux-593D88?style=for-the-badge&logo=redux&logoColor=white'
+    }
+  },
+  {
+    a: { target: '_blank', rel: 'noopener noreferrer', href: 'https://babeljs.io/' }
+    , img: {
+      alt: 'Babel',
+      src: 'https://img.shields.io/badge/Babel-eccc04?style=for-the-badge&logo=babel&logoColor=white'
+    }
+  },
+  // {
+  //   a: {
+  //     target: '_blank',
+  //     rel: 'noopener noreferrer'
+  //     , href: 'https://www.gatsbyjs.com/'
+  //   }
+  //   , img: {
+  //     alt: 'Gatsby',
+  //     src: 'https://img.shields.io/badge/Gatsby-663399?style=for-the-badge&logo=gatsby&logoColor=white'
+  //   }
+  // },
+  {
+    a: {
+      target: '_blank',
+      rel: 'noopener noreferrer'
+      , href: 'https://www.styled-components.com/'
+    }
+    , img: {
+      alt: 'Styled Components',
+      src: 'https://img.shields.io/badge/styled--components-DB7093?style=for-the-badge&logo=styled-components&logoColor=white'
+    }
+  }
+]
+
+export const ToolsLogos = [
+  {
+    a: {
+      target: '_blank', rel: 'noopener noreferrer', href: 'https://slack.com/'
+    },
+    img: {
+      alt: 'Slack'
+      , src: 'https://img.shields.io/badge/Slack-4A154B?style=for-the-badge&logo=slack&logoColor=white'
+    }
+  },
+  {
+    a: {
+      target: '_blank', rel: 'noopener noreferrer', href: 'https://jestjs.io/'
+    },
+    img: {
+      alt: 'Jest'
+      , src: 'https://img.shields.io/badge/Jest-9d475f?style=for-the-badge&logo=jest&logoColor=white'
+    }
+  },
+  {
+    a: {
+      target: '_blank', rel: 'noopener noreferrer', href: 'https://gitlab.com/'
+    },
+    img: {
+      alt: 'Gitlab', src: 'https://img.shields.io/badge/GitLab-330F63?style=for-the-badge&logo=gitlab&logoColor=white'
+    }
+  },
+  {
+    a: {
+      target: '_blank', rel: 'noopener noreferrer', href: 'https://postman.com/'
+    },
+    img: {
+      alt: 'Postman'
+      , src: 'https://img.shields.io/badge/Postman-FF6C37?style=for-the-badge&logo=postman&logoColor=white'
+    }
+  },
+  {
+    a: {
+      target: '_blank', rel: 'noopener noreferrer', href: 'https://zoom.us/'
+    },
+    img: {
+      alt: 'Zoom'
+      , src: 'https://img.shields.io/badge/Zoom-2D8CFF?style=for-the-badge&logo=zoom&logoColor=white'
+    }
+  },
+  {
+    a: {
+      target: '_blank', rel: 'noopener noreferrer', href: 'https://codepen.com/'
+    },
+    img: {
+      alt: 'Codepen'
+      , src: 'https://img.shields.io/badge/Codepen-000000?style=for-the-badge&logo=codepen&logoColor=white'
+    }
+  },
+  {
+    a: {
+      target: '_blank',
+      rel: 'noopener noreferrer',
+      href: 'https://code.visualstudio.com'
+    },
+    img: {
+      alt: 'VS Code'
+      , src: 'https://img.shields.io/badge/Visual_Studio_Code-0078d7?style=for-the-badge&logo=visual-studio&logoColor=white'
+
+    }
+  },
+  {
+    a: {
+      target: '_blank',
+      rel: 'noopener noreferrer',
+      href: 'https://git-scm.com/about'
+    },
+    img: {
+      alt: 'Git'
+      , src: 'https://img.shields.io/badge/Git-dc4c34?style=for-the-badge&logo=git&logoColor=white'
+
+    }
+  },
+
+  {
+    a: {
+      target: '_blank', rel: 'noopener noreferrer', href: 'https://rspec.info/'
+    },
+    img: {
+      alt: 'Rspec'
+      , src: 'https://img.shields.io/badge/Rspec-ec2861?style=for-the-badge&logo=ruby&logoColor=white'
+
+    }
+  },
+  {
+    a: {
+      target: '_blank',
+      rel: 'noopener noreferrer',
+      href: 'https://darkreader.org/'
+    },
+    img: {
+      alt: 'Dark Reader'
+      , src: 'https://img.shields.io/badge/Dark_Reader-141E24?style=for-the-badge&logo=dark-reader&logoColor=white'
+    }
+  },
+  {
+    a: {
+      target: '_blank', rel: 'noopener noreferrer', href: 'https://github.com/'
+    },
+    img: {
+      alt: 'Github'
+      , src: 'https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white'
+
+    }
+  },
+]
